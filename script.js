13381bc8f08633311ae7a99b1c34078c9389adcd
aban-007 <abanaban8200@gmail.com>
2026-05-14 07:38:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,22 +5,15 @@ showSlides();
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("insta-slide");
-    
-    // Hide all slides
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
-    
     slideIndex++;
-    
-    // Reset index if it exceeds number of slides
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }    
+    slides[slideIndex-1].style.display = "block";  
     
-    // Show current slide
-    slides[slideIndex - 1].style.display = "block";  
-    
-    // CHANGE TIME (5000ms = 5 Seconds)
+    // WAKTU PERGANTIAN (5000ms = 5 Detik)
     setTimeout(showSlides, 5000); 
 }
