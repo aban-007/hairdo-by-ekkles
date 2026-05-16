50c580ddc84ce1937d146f7b211778f92428e9f2
aban-007 <abanaban8200@gmail.com>
2026-05-17 03:11:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ function showSlides() {
         slides[i].style.display = "none"; 
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}   
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }   
     slides[slideIndex-1].style.display = "block"; 
     
     // WAKTU PERGANTIAN (5000ms = 5 Detik)
